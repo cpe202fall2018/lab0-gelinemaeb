b1684da03aebd8518c7521c5cf1fff0ab5f34a21
gelinemaeb <ebarruel@calpoly.edu>
2018-09-24 19:42:26 -0700

    probs done but adding more tests

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
-   # write your code here
+    w = float(input("What do you weigh on earth? "))
+    m = w * 0.38
+    j = w * 2.34
    
-   
+    print("\nOn Mars you would weigh " + str(m) + " pounds.\nOn Jupiter you would weigh " + str(j) + " pounds.")
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
--- planets_tests.py
+++ planets_tests.py
@@ -12,6 +12,26 @@ class Test_planets(unittest.TestCase):
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
 
+    def test_02(self):
+        sys.stdin = io.StringIO("1")
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.38 pounds.\nOn Jupiter you would weigh 2.34 pounds."
+        weight_on_planets()
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+    def test_03(self):
+        sys.stdin = io.StringIO("0")
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.0 pounds.\nOn Jupiter you would weigh 0.0 pounds."
+        weight_on_planets()
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+    def test_04(self):
+        sys.stdin = io.StringIO("-1")
+        sys.stdout = student_output = io.StringIO()
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh -0.38 pounds.\nOn Jupiter you would weigh -2.34 pounds."
+        weight_on_planets()
+        self.assertEqual(expected_out, student_output.getvalue().strip())
 
 if __name__ == "__main__":
         unittest.main()
